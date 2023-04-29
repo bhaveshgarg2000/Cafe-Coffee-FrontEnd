de21af502bf7612cb35bd149528ed6d85be4af73
bhaveshgarg2000 <bhaveshgarg2005@gmail.com>
2023-04-29 17:21:06 +0530

    Frontend

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <img style={{width: '100%'}} src="https://cdnimg.webstaurantstore.com/uploads/blog/2019/4/coffee-drinks-infographic.png" />
+        );
+    }
+}
+
+export default Home;
